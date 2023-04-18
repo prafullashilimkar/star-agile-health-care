55552c17bccb1ace88065eb4a93f3b8eab4531b4
prafullashilimkar <122914577+prafullashilimkar@users.noreply.github.com>
2023-04-18 20:14:43 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "deployment-server" {
+ resource "aws_instance" "deployment-server" {
  ami = "ami-02eb7a4783e7e9317"
  instance_type = "t2.medium"
- availability_zone = "ap-south-1a"
- vpc_security_group_ids = [aws_security_group.mysg1.id]
+ availability_zone = "ap-south-1"
+ vpc_security_group_ids = "sg-08285106aef78eb40"
  key_name = "jenkinskey1"
  tags = {
  name = "kubernetes_instance"
@@ -15,7 +15,8 @@ resource "aws_instance" "deployment-server" {
  "sudo chmod +x /home/ubuntu/minikube-linux-amd64",
  "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
  "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s 
-https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
+  https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
  "sudo chmod +x /home/ubuntu/kubectl",
  "sudo cp kubectl /
+ }
 
