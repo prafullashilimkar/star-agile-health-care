801608be56fae7ce870787efcad0d7ebfa2dda8e
prafullashilimkar <122914577+prafullashilimkar@users.noreply.github.com>
2023-04-21 23:28:32 +0530

    Update provider.tf

--- my-serverfiles/provider.tf
+++ my-serverfiles/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.11.0"
+    }
+  }
+}
 provider "aws" {
 region     = "ap-south-1" 
+access_key = "AKIAUQHHECWTCN3JEJLW"
+secret_key = "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" 
 }
