a01b83b560b281fee748a0b62bb146231753b37d
prafullashilimkar <122914577+prafullashilimkar@users.noreply.github.com>
2023-04-21 23:38:06 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,24 +1,21 @@
 resource "aws_instance" "test-server" {
   ami           = "ami-02eb7a4783e7e9317" 
   instance_type = "t2.medium" 
-  key_name = "frstkey"
+  availability_zone = "ap-south-1a"
   vpc_security_group_ids= [aws_security_group.my_sg.id]
+  key_name = "jenkinskey1.pem"
+  
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("frstkey.pem")
-    # host     = self.public_ip
-    host = aws_instance.instance.public_ip
-   
-  }
-  provisioner "remote-exec" {
-    inline = [ "echo 'wait to start instance' "]
-  }
+    private_key = file("jenkinskey1.pem")
+    host     = self.public_ip
+  }  
+  
   tags = {
     Name = "test-server"
   }
-  provisioner "local-exec" {
-        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+  
   }
    provisioner "remote-exec" {
      inline = [
