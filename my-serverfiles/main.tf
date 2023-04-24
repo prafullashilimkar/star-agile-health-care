beab4a249205a027956d33c5d021ee432d45e8ea
prafullashilimkar <122914577+prafullashilimkar@users.noreply.github.com>
2023-04-24 21:51:45 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -3,12 +3,12 @@ resource "aws_instance" "test-server" {
   instance_type = "t2.medium" 
   availability_zone = "ap-south-1a"
   vpc_security_group_ids= [aws_security_group.my_sg.id]
-  key_name = "jenkinskey1"
+  key_name = "serverkey"
   
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("jenkinskey1.pem")
+    private_key = file("serverkey.pem")
     host     = self.public_ip
   }  
   
@@ -24,8 +24,8 @@ resource "aws_instance" "test-server" {
                "sudo snap install microk8s --clasic",
                "sudo sleep 30",
                "sudo microk8s status",
-               "sudo microk8s kubectl create deployment medicure-deploy --image=shankerchauhan/projects:healthcare1",
-               "sudo microk8s kubectl expose deployment medicure-deploy --port=8082 --type=NodePort",
+               "sudo microk8s kubectl create deployment medicure-deploy --image=prafullla/healthcare-app:latest",
+               "sudo microk8s kubectl expose deployment medicure-deploy --port=8084 --type=NodePort",
                "sudo microk8s kubectl get svc",
                "sudo echo public IP Address of the Instance",
                "sudo curl http://checkip.amazonaws.com",
